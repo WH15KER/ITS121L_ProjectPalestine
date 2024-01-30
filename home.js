562d1240699c3258bb407355dfceb5725069aaf8
Lockwood <nelvinsp326@gmail.com>
2024-01-31 05:47:26 +0800

    Updated Homepage

--- home.js
+++ home.js
@@ -1,15 +1,34 @@
 let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector('.navbar a');
 
 menu.onclick = () =>{
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 }
 
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header .navbar a');
+
+
 window.onscroll = () =>{
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
+
+  section.forEach(sec =>{
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetHeight - 150;
+    let id = sec.getAttribute('id');
+
+    if(top => offset && top < offset + height){
+      navlinks.forEach(link =>{
+        link.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+  });
 }
+
 
 
 document.querySelector('#search-icon').onclick = () =>{
@@ -57,4 +76,14 @@ var swiper = new Swiper(".review-slider", {
     },
   },
 });
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 2700);
+}
+
+window.onload = fadeOut;
 
